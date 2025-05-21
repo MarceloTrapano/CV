23e022241db4270eb2dde65c64349746fc5b6364
MarceloTrapano <kacper7866@gmail.com>
2025-05-21 12:39:50 +0200

    Updating project

--- Project2/live.py
+++ Project2/live.py
@@ -3,48 +3,90 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from utils import threshold, ball_separator
-def show_balls(frame, balls, inner_color, outher_color, low=50, high=400):
-    contours, _ = cv2.findContours(balls, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    filtered_contours = [c for c in contours if low < cv2.contourArea(c) < high]
-    if contours:
-# Zakładamy, że największy kontur to kulka
-        for c in filtered_contours:
-        # Oblicz momenty
-            M = cv2.moments(c)
+from typing import List, Any
+from cv2.typing import MatLike
+from numpy.typing import NDArray
 
+def show_balls(frame, balls, inner_color, outher_color, low=50, high=400, show=True) -> NDArray[Any]:
+    contours, _ = cv2.findContours(balls, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    filtered_contours: List[Any] = [c for c in contours if low < cv2.contourArea(c) < high]
+    balls_coordinates: List[Any] = []
+    if contours:
+        for c in filtered_contours:
+            M = cv2.moments(c)
             if M["m00"] != 0:
-                # Centrum kulki:
                 cx = int(M["m10"] / M["m00"])
                 cy = int(M["m01"] / M["m00"])
 
                 if cx < 125 or cx > 1175 or cy < 95 or cy > 615:
                     continue
-                # Opcjonalnie: narysuj punkt
-                cv2.circle(frame, (cx, cy), 20, outher_color, 1)
-                cv2.circle(frame, (cx, cy), 1, inner_color, 2)
-def main():
+                if show:
+                    cv2.circle(frame, (cx, cy), 15, outher_color, 1)
+                    cv2.circle(frame, (cx, cy), 1, inner_color, 2)
+                balls_coordinates.append([cx, cy])
+        return np.array(balls_coordinates)
+    return np.array([])
+def track_ball(coordinates, start_coordinate, frame, color, show=False) -> List[int]:
+    if coordinates.shape[0] != 0:
+        if show:
+            print(coordinates[np.argmin(np.linalg.norm(coordinates - start_coordinate, axis=1))])
+        start_coordinate = coordinates[np.argmin(np.linalg.norm(coordinates - start_coordinate, axis=1))]
+        cv2.circle(frame, (start_coordinate[0], start_coordinate[1]), 15, color, 1)
+        cv2.circle(frame, (start_coordinate[0], start_coordinate[1]), 1, color, 2)
+    return start_coordinate
+
+def main() -> None:
+    # Tworzenie obiektu do przechwytywania wideo
     video = cv2.VideoCapture('video.mp4')
+
+    # Inicjacja zapisu współrzędnych bil
+    track_white: List[int] = [0, 0]
+    track_blue: List[int] = [0, 0]
+    track_green: List[int] = [0, 0]
+    track_pink: List[int] = [0, 0]
+    track_brown: List[int] = [0, 0]
+    track_yellow: List[int] = [0, 0]
+    track_black: List[int] = [0, 0]
+
     while(video.isOpened()):
+        # Czytanie klatki
         ret, frame = video.read()
+
         if ret == True:
-            white_ball = ball_separator(frame,"white")
-            red_balls = ball_separator(frame, "red")
-            yellow_ball = ball_separator(frame, "yellow")
-            pink_ball = ball_separator(frame,"pink")
-            green_ball = ball_separator(frame,"green")
-            black_ball = ball_separator(frame,"black")
-            blue_ball = ball_separator(frame,"blue")
-            brown_ball = ball_separator(frame, "brown")
-            show_balls(frame, white_ball, (142, 142, 142), (142, 142, 142))
-            show_balls(frame, blue_ball, (253, 128, 0), (253, 128, 0), low=0)
-            show_balls(frame, green_ball, (0, 255, 0), (0, 255, 0), low=70)
-            show_balls(frame, pink_ball, (180, 175, 236), (180, 175, 236))
-            show_balls(frame, brown_ball, (0, 78, 149), (0, 78, 149), low=150)
-            show_balls(frame, yellow_ball, (0, 241, 253), (0, 241, 253))
-            show_balls(frame, black_ball, (0, 0, 0), (0, 0, 0))
-            show_balls(frame, red_balls, (0, 0, 255), (0, 0, 255))
+            # Ekstrakcja bil z obrazu
+            white_ball: MatLike = ball_separator(frame,"white")
+            red_balls: MatLike = ball_separator(frame, "red")
+            yellow_ball: MatLike = ball_separator(frame, "yellow")
+            pink_ball: MatLike = ball_separator(frame,"pink")
+            green_ball: MatLike = ball_separator(frame,"green")
+            black_ball: MatLike = ball_separator(frame,"black")
+            blue_ball: MatLike = ball_separator(frame,"blue")
+            brown_ball: MatLike = ball_separator(frame, "brown")
+
+            # Wyciąganie współrzędnych bil
+            white_coor: NDArray[Any] = show_balls(frame, white_ball, (142, 142, 142), (142, 142, 142), show=False)
+            blue_coor: NDArray[Any] = show_balls(frame, blue_ball, (253, 128, 0), (253, 128, 0), low=0, show=False)
+            green_coor: NDArray[Any] = show_balls(frame, green_ball, (0, 255, 0), (0, 255, 0), low=70, show=False)
+            pink_coor: NDArray[Any] = show_balls(frame, pink_ball, (180, 175, 236), (180, 175, 236), show=False)
+            brown_coor: NDArray[Any] = show_balls(frame, brown_ball, (0, 78, 149), (0, 78, 149), low=150, show=False)
+            yellow_coor: NDArray[Any] = show_balls(frame, yellow_ball, (0, 241, 253), (0, 241, 253), show=False)
+            black_coor: NDArray[Any] = show_balls(frame, black_ball, (0, 0, 0), (0, 0, 0), show=False)
+            red_coor: NDArray[Any] = show_balls(frame, red_balls, (0, 0, 255), (0, 0, 255))
+
+            # Śledzenie bil
+            track_white = track_ball(white_coor, track_white, frame, (142, 142, 142), show=True)
+            track_blue = track_ball(blue_coor, track_blue, frame, (253, 128, 0))
+            track_green = track_ball(green_coor, track_green, frame, (0, 255, 0))
+            track_pink = track_ball(pink_coor, track_pink, frame, (180, 175, 236))
+            track_brown = track_ball(brown_coor, track_brown, frame, (0, 78, 149))
+            track_yellow = track_ball(yellow_coor, track_yellow, frame, (0, 241, 253))
+            track_black = track_ball(black_coor, track_black, frame, (0, 0, 0))
+
+            # Pokazywanie zedytowanej klatki
             cv2.imshow('Frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+
+            # Zamknięcie programu po kliknięciu "q"
+            if  cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             break
--- Project2/utils.py
+++ Project2/utils.py
@@ -14,10 +14,8 @@ def ball_separator(image, color):
             sep_2 = cv2.threshold(image[:,:,2], 80, 255, cv2.THRESH_BINARY)[1]
             return cv2.bitwise_and(sep_1,sep_2)
         case "pink":
-            sep_1 = cv2.threshold(image[:,:,0], 60, 255, cv2.THRESH_BINARY)[1]
-            sep_2 = cv2.threshold(image[:,:,2], 220, 255, cv2.THRESH_BINARY)[1]
-            sep_3 = cv2.threshold(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), 180, 255, cv2.THRESH_BINARY)[1]
-            return cv2.morphologyEx(cv2.bitwise_and(sep_1,sep_2)- sep_3, cv2.MORPH_OPEN, kernel=np.ones((3,3)))
+            sep_3 = threshold(cv2.cvtColor(image, cv2.COLOR_RGB2HSV)[:,:,1], 120, 180)
+            return cv2.morphologyEx(sep_3, cv2.MORPH_OPEN, kernel=np.ones((3,3)))
         case "blue":
             sep_1 = cv2.threshold(image[:,:,0], 90, 255, cv2.THRESH_BINARY)[1]
             sep_2 = cv2.threshold(image[:,:,2], 23, 255, cv2.THRESH_BINARY_INV)[1]
@@ -27,10 +25,8 @@ def ball_separator(image, color):
             sep_2 = cv2.threshold(image[:,:,2], 15, 255, cv2.THRESH_BINARY_INV)[1]
             return cv2.bitwise_and(sep_1,sep_2)
         case "green": # tego sie nie da
-            sep_1 = cv2.threshold(image[:,:,0], 40, 255, cv2.THRESH_BINARY)[1]
-            sep_2 = threshold(image[:,:,1], 110, 158)
-            sep = cv2.bitwise_and(sep_1,sep_2)
-            return cv2.dilate(cv2.morphologyEx(np.clip(sep.astype(int) - ball_separator(image, "pink").astype(int) - ball_separator(image, "blue").astype(int), 0, 255).astype(np.uint8), cv2.MORPH_OPEN, kernel=np.ones((3,3))), kernel=np.ones((3,3)))
+            sep = threshold(cv2.cvtColor(image, cv2.COLOR_RGB2HSV)[:,:,0], 44, 57)
+            return cv2.morphologyEx(sep, cv2.MORPH_OPEN, kernel=np.ones((3,3)))
         case "yellow":
             sep_2 = threshold(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), 150, 200)
             sep_1 = cv2.threshold(image[:,:,0], 80, 255, cv2.THRESH_BINARY)[1]
@@ -44,7 +40,7 @@ def ball_separator(image, color):
         case "brown":
             sep_1 = threshold(image[:,:,2], 60, 250)
             sep_2 = cv2.threshold(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), 100, 255, cv2.THRESH_BINARY_INV)[1]
-            sep_3 = threshold(image[:,:,1], 0, 170)
+            sep_3 = threshold(image[:,:,1], 85, 160)
             return cv2.morphologyEx(cv2.bitwise_and(cv2.bitwise_and(sep_1,sep_2), sep_3), cv2.MORPH_OPEN, kernel=np.ones((3,3)))
         case _:
             raise ValueError
